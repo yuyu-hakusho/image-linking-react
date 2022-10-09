255451d8ad13ed913038739528c41a86ae5d4aaf
Kyle Coberly <kyle.coberly@gmail.com>
2022-10-09 16:17:41 -0600

    Remove solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import drakeYes from './drake-yes.jpg';
 
 function App() {
   return (
     <div className="App">
-      <img className="no" alt="Drake saying no meme" src="/drake-no.jpg" />
-      <img className="yes" alt="Drake saying yes meme" src={drakeYes} />
+      <img className="no" alt="Drake saying no meme" />
+      <img className="yes" alt="Drake saying yes meme" />
     </div>
   );
 }
