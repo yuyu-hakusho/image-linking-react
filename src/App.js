0de6c299b9fa72ad297705e6b67b7deb1affd334
Jennifer-Tsang_lmig <jennifer.tsang@libertymutual.com>
2023-06-10 23:24:34 -0400

    add to github

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import drakeyes from "./drake-yes.jpg";
 
 function App() {
   return (
     <div className="App">
-      <img className="no" alt="Drake saying no meme" />
-      <img className="yes" alt="Drake saying yes meme" />
+      <img className="no" src="./image/drake-no.jpg" alt="Drake saying no meme" />
+      <img className="yes" src={drakeyes} alt="Drake saying yes meme" />
     </div>
   );
 }
